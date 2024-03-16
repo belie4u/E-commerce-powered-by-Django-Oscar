2d926ed3118cbf9b68d1f3e84c43f495dec2c1aa
ElieCode <byiringiro4u@gmail.com>
2024-03-16 15:23:26 +0200

    categories facet solr

---
+++ facet_data.py
@@ -0,0 +1,39 @@
+facet_data = {
+    "product_class": {
+        'name': 'PRODUCT TYPES',
+        'results': [
+            {'name': 'T-shirt', 'count': 190, 'show_count': True, 'disabled': False, 'selected': False, 'select_url': '/?selected_facets=product_class_exact%3AT-shirt'}
+        ]
+    },
+    "rating": {
+        'name': 'RATINGS',
+        'results': [
+            {'name': '1', 'count': 0, 'show_count': True, 'disabled': True, 'selected': False, 'select_url': '/?selected_facets=rating_exact%3A1'},
+            {'name': '2', 'count': 1, 'show_count': True, 'disabled': False, 'selected': False, 'select_url': '/?selected_facets=rating_exact%3A2'},
+            {'name': '3', 'count': 1, 'show_count': True, 'disabled': False, 'selected': False, 'select_url': '/?selected_facets=rating_exact%3A3'},
+            {'name': '4', 'count': 0, 'show_count': True, 'disabled': True, 'selected': False, 'select_url': '/?selected_facets=rating_exact%3A4'},
+            {'name': '5', 'count': 1, 'show_count': True, 'disabled': False, 'selected': False, 'select_url': '/?selected_facets=rating_exact%3A5'}
+        ]
+    },
+    "price_range": {
+        'name': 'PRICE RANGE',
+        'results': [
+            {'name': '0 to 20', 'count': 132, 'show_count': True, 'disabled': False, 'selected': False, 'select_url': '/?selected_facets=price_exact%3A%5B0+TO+20%5D'},
+            {'name': '20 to 40', 'count': 57, 'show_count': True, 'disabled': False, 'selected': False, 'select_url': '/?selected_facets=price_exact%3A%5B20+TO+40%5D'},
+            {'name': '40 to 60', 'count': 0, 'show_count': True, 'disabled': True, 'selected': False, 'select_url': '/?selected_facets=price_exact%3A%5B40+TO+60%5D'},
+            {'name': '60+', 'count': 0, 'show_count': True, 'disabled': True, 'selected': False, 'select_url': '/?selected_facets=price_exact%3A%5B60+TO+%2A%5D'}
+        ]
+    },
+    "color": {
+        'name': 'COLOR',
+        'results': [
+            {'name': 'Green', 'count': 0, 'show_count': True, 'disabled': True, 'selected': False, 'select_url': '/?selected_facets=color_exact%3AGreen'},
+            {'name': 'Grey', 'count': 0, 'show_count': True, 'disabled': True, 'selected': False, 'select_url': '/?selected_facets=color_exact%3AGrey'},
+            {'name': 'Blue', 'count': 0, 'show_count': True, 'disabled': True, 'selected': False, 'select_url': '/?selected_facets=color_exact%3ABlue'},
+            {'name': 'Black', 'count': 0, 'show_count': True, 'disabled': True, 'selected': False, 'select_url': '/?selected_facets=color_exact%3ABlack'},
+            {'name': 'Yellow', 'count': 0, 'show_count': True, 'disabled': True, 'selected': False, 'select_url': '/?selected_facets=color_exact%3AYellow'},
+            {'name': 'Red', 'count': 0, 'show_count': True, 'disabled': True, 'selected': False, 'select_url': '/?selected_facets=color_exact%3ARed'},
+            {'name': 'White', 'count': 190, 'show_count': True, 'disabled': False, 'selected': False, 'select_url': '/?selected_facets=color_exact%3AWhite'}
+        ]
+    }
+}
